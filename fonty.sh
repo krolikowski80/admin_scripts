e11e8b39c87387540005be55aebc4d395e9186d4
Tomasz Królikowski <t.krolikowski@gmail.com>
2020-08-26 19:29:14 +0200

    zmiany

--- fonty.sh
+++ fonty.sh
@@ -5,7 +5,8 @@ mkdir -p /tmp/adodefont
 cd /tmp/adodefont
 wget -q --show-progress -O source-code-pro.zip https://github.com/adobe-fonts/source-code-pro/archive/2.030R-ro/1.050R-it.zip
 unzip -q source-code-pro.zip -d source-code-pro
-mkdir -p ~/.fonts
-cp -v source-code-pro/*/OTF/*.otf ~/.fonts/
+fontpath="${XDG_DATA_HOME:-$HOME/.local/share}"/fonts
+mkdir -p $fontpath
+cp -v source-code-pro/*/OTF/*.otf $fontpath
 fc-cache -f
 rm -rf source-code-pro{,.zip}
