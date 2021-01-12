9d621f16ac2ab92bb7e4c4578e9fdc9f42c3d9bd
Tomasz Królikowski <t.krolikowski@gmail.com>
2021-01-12 13:23:49 +0100

    poprawki

--- pyenv-install.sh
+++
@@ -1,7 +0,0 @@
-sudo apt update
-sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
-libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
-libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl
-
-curl https://pyenv.run | bash
-
