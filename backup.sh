f2394b139aabab542a648edf2e86b42115f00ce5
Tomasz Królikowski <t.krolikowski@gmail.com>
2021-03-10 19:31:08 +0100

    backup.sh

--- backup.sh
+++ backup.sh
@@ -1,18 +1,15 @@
-#/bin/bash
-
-rsync -aAXv \
+sudo rsync -aAXvz \
+--log-file=/home/tomasz/rsync_log.txt \
 --delete \
---prune-empty-dirs \
---include='/*/' \
---include='/*/*/' \
---include='/home/tomasz/Documents/***' \
---include='/home/tomasz/.bashrc' \
---include='/home/tomasz/Biblioteka\ calibre/***' \
---include='/home/tomasz/Desktop/***' \
---include='/home/tomasz/dev_tools/***' \
---include='/home/tomasz/.gitconfig' \
---include='/home/tomasz/Music/***' \
---include='/home/tomasz/Pictures/***' \
---include='/home/tomasz/Videos/kursy/***' \
---exclude='*' / /media/tomasz/Przenosny/tomek_backup/
+--include='Dokumenty/***' \
+--include='Biblioteka\ calibre/***' \
+--include='local_repo/***' \
+--include='Obrazy/***' \
+--include='Pulpit/***' \
+--include='.ssh/***' \
+--include='.vimrc' \
+--include='.bash*' \
+--exclude='*' \
+/home/tomasz/ /mnt/dysk_zew/home_backup
+
 
