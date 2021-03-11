007fc31692a61cd84f872c40b0a3dcf2cfcce36e
Tomasz Królikowski <t.krolikowski@gmail.com>
2021-03-11 23:47:00 +0100

    zmiany

--- backup.sh
+++ backup.sh
@@ -3,13 +3,12 @@ sudo rsync -aAXvz \
 --delete \
 --include='Dokumenty/***' \
 --include='Biblioteka\ calibre/***' \
---include='local_repo/***' \
 --include='Obrazy/***' \
 --include='Pulpit/***' \
 --include='.ssh/***' \
 --include='.vimrc' \
 --include='.bash*' \
 --exclude='*' \
-/home/tomasz/ /mnt/dysk_zew/home_backup
+/home/tomasz/ /run/media/tomasz/049c3674-02ec-4f18-b057-84f8bf540ea9/home_backup
 
 
